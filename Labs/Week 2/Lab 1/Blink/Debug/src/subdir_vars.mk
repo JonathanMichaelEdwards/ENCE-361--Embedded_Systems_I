529d86e70b876b5460b82e0b25eab39b64bdf30c
JonathanMichaelEdwards <jon.michael.edwardws13@gmil.com>
2020-02-24 22:20:04 +1300

    debugger mode

---
+++ Labs/Week 2/Lab 1/Blink/Debug/src/subdir_vars.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/DutyCycleBlinky.c \
+../src/delay.c 
+
+C_DEPS += \
+./src/DutyCycleBlinky.d \
+./src/delay.d 
+
+OBJS += \
+./src/DutyCycleBlinky.obj \
+./src/delay.obj 
+
+OBJS__QUOTED += \
+"src\DutyCycleBlinky.obj" \
+"src\delay.obj" 
+
+C_DEPS__QUOTED += \
+"src\DutyCycleBlinky.d" \
+"src\delay.d" 
+
+
